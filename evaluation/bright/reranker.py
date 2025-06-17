aeb3b7c992c98e425b02940e4288e6f235f796be
Alexander Rubinstein <rubalex14@gmail.com>
2025-06-17 17:08:56 +0200

    Update reranker.py formatting

--- evaluation/bright/reranker.py
+++ evaluation/bright/reranker.py
@@ -9,9 +9,19 @@ import torch
 import prompts
 import sys
 
-sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
-from utility.utils import get_device_with_most_free_memory
+sys.path.insert(
+    0,
+    os.path.join(
+        os.path.dirname(
+            os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        ),
+        "utility",
+    ),
+)
+from utils import get_device_with_most_free_memory
+
 sys.path.pop(0)
+
 
 class Reranker:
     def __init__(self, task, device_count=None, device=None):
@@ -20,22 +30,24 @@ class Reranker:
             device_count = torch.cuda.device_count()
         if device is None:
             device = get_device_with_most_free_memory()
-        self.sampling_params = SamplingParams(temperature=0.6, top_p=0.9, max_tokens=32, logprobs=5)
+        self.sampling_params = SamplingParams(
+            temperature=0.6, top_p=0.9, max_tokens=32, logprobs=5
+        )
         self.model = LLM(
             model=model_name,
             dtype="bfloat16",
             tensor_parallel_size=device_count,
             max_model_len=16384,
-            device=device
+            device=device,
         )
 
         retrieval_dict = {
-                "aops": prompts.bright_aops,
-                "theoremqa_questions": prompts.bright_theoremqa_questions,
-                "leetcode": prompts.bright_leetcode,
-                "pony": prompts.bright_pony,
-                "theoremqa_theorems": prompts.bright_theoremqa_theorems
-            }
+            "aops": prompts.bright_aops,
+            "theoremqa_questions": prompts.bright_theoremqa_questions,
+            "leetcode": prompts.bright_leetcode,
+            "pony": prompts.bright_pony,
+            "theoremqa_theorems": prompts.bright_theoremqa_theorems,
+        }
         if args.task in retrieval_dict:
             self.prompt = retrieval_dict[task]
         else:
@@ -48,9 +60,11 @@ class Reranker:
         scores = []
 
         for i in tqdm(range(0, len(docs), batch_size)):
-            list_docs = docs[i:i+batch_size]
+            list_docs = docs[i : i + batch_size]
 
-            doc_prompts = [self.prompt.format(query, doc["text"]) for doc in list_docs]
+            doc_prompts = [
+                self.prompt.format(query, doc["text"]) for doc in list_docs
+            ]
 
             output = self.model.generate(doc_prompts, self.sampling_params)
             doc_prompt_outputs = [o.outputs[0].text for o in output]
@@ -60,7 +74,14 @@ class Reranker:
                 if pos_score != -1:
                     try:
                         # get the score from the prompt output
-                        score = float(doc_prompt_outputs[j][pos_score+16:pos_score+18])/5
+                        score = (
+                            float(
+                                doc_prompt_outputs[j][
+                                    pos_score + 16 : pos_score + 18
+                                ]
+                            )
+                            / 5
+                        )
                         scores.append(score)
                     except:
                         print("In exception!! {}".format(doc_prompt_outputs[j]))
@@ -68,44 +89,73 @@ class Reranker:
                 else:
                     scores.append(0)
 
-
         ranking = {doc["id"]: score for doc, score in zip(docs, scores)}
-        ranking = dict(sorted(ranking.items(), key=lambda item: item[1], reverse=True)[:topk])
+        ranking = dict(
+            sorted(ranking.items(), key=lambda item: item[1], reverse=True)[
+                :topk
+            ]
+        )
         return ranking
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--task', type=str, required=True,
-                        choices=['biology','earth_science','economics','pony','psychology','robotics','theoremqa_questions', "theoremqa_theorems",
-                                 'stackoverflow','sustainable_living','aops','leetcode'])
-    parser.add_argument('--long_context', action='store_true')
-    parser.add_argument('--retriever_score_file', type=str, default=None)
-    parser.add_argument('--input_k', type=int)
-    parser.add_argument('--k', type=int)
-    parser.add_argument('--cache_dir', type=str, default='cache')
-    parser.add_argument('--reasoning', type=str, default=None)
-    parser.add_argument('--bm25_score_file', type=str, default=None)
-    parser.add_argument('--output_dir', type=str, default=None)
-    parser.add_argument('--batch_size', type=int, default=50)
+    parser.add_argument(
+        "--task",
+        type=str,
+        required=True,
+        choices=[
+            "biology",
+            "earth_science",
+            "economics",
+            "pony",
+            "psychology",
+            "robotics",
+            "theoremqa_questions",
+            "theoremqa_theorems",
+            "stackoverflow",
+            "sustainable_living",
+            "aops",
+            "leetcode",
+        ],
+    )
+    parser.add_argument("--long_context", action="store_true")
+    parser.add_argument("--retriever_score_file", type=str, default=None)
+    parser.add_argument("--input_k", type=int)
+    parser.add_argument("--k", type=int)
+    parser.add_argument("--cache_dir", type=str, default="cache")
+    parser.add_argument("--reasoning", type=str, default=None)
+    parser.add_argument("--bm25_score_file", type=str, default=None)
+    parser.add_argument("--output_dir", type=str, default=None)
+    parser.add_argument("--batch_size", type=int, default=50)
     args = parser.parse_args()
 
     if args.reasoning is not None:
-        raw_examples = load_dataset('xlangai/bright', f"{args.reasoning}_reason", cache_dir=args.cache_dir)[args.task]
+        raw_examples = load_dataset(
+            "xlangai/bright",
+            f"{args.reasoning}_reason",
+            cache_dir=args.cache_dir,
+        )[args.task]
     else:
-        raw_examples = load_dataset('xlangai/bright', 'examples',cache_dir=args.cache_dir)[args.task]
+        raw_examples = load_dataset(
+            "xlangai/bright", "examples", cache_dir=args.cache_dir
+        )[args.task]
 
     # raw_examples = load_dataset('xlangai/bright', 'examples', cache_dir=args.cache_dir)[args.task]
     examples = {}
     for e in raw_examples:
-        examples[e['id']] = e
+        examples[e["id"]] = e
     if args.long_context:
-        doc_pairs = load_dataset('xlangai/bright', 'long_documents', cache_dir=args.cache_dir)[args.task]
+        doc_pairs = load_dataset(
+            "xlangai/bright", "long_documents", cache_dir=args.cache_dir
+        )[args.task]
     else:
-        doc_pairs = load_dataset('xlangai/bright', 'documents', cache_dir=args.cache_dir)[args.task]
+        doc_pairs = load_dataset(
+            "xlangai/bright", "documents", cache_dir=args.cache_dir
+        )[args.task]
     documents = {}
     for d in doc_pairs:
-        documents[d['id']] = d['content']
+        documents[d["id"]] = d["content"]
 
     with open(args.retriever_score_file) as f:
         all_scores = json.load(f)
@@ -116,21 +166,27 @@ if __name__=='__main__':
     if not os.path.isfile(score_file_path):
         new_scores = copy.deepcopy(all_scores)
 
-        model = Reranker(args.task, device_count=1, device=get_device_with_most_free_memory())
+        model = Reranker(
+            args.task, device_count=1, device=get_device_with_most_free_memory()
+        )
 
-        for qid,scores in tqdm(all_scores.items()):
+        for qid, scores in tqdm(all_scores.items()):
             docs = []
-            sorted_scores = sorted(scores.items(),key=lambda x:x[1],reverse=True)[:args.input_k]
+            sorted_scores = sorted(
+                scores.items(), key=lambda x: x[1], reverse=True
+            )[: args.input_k]
             for did, _ in sorted_scores:
                 docs.append([did, documents[did]])
 
-            ctxs = [{'id': did, 'text': documents[did]} for did, _ in sorted_scores]
+            ctxs = [
+                {"id": did, "text": documents[did]} for did, _ in sorted_scores
+            ]
 
             cur_score = model.rerank(
-                query=examples[qid]['query'],
+                query=examples[qid]["query"],
                 docs=ctxs,
                 topk=args.k,
-                batch_size=args.batch_size
+                batch_size=args.batch_size,
             )
 
             assert len(cur_score) == len(sorted_scores)
@@ -138,31 +194,33 @@ if __name__=='__main__':
             new_scores[qid] = cur_score
 
         os.makedirs(outputs_path, exist_ok=True)
-        with open(score_file_path, 'w') as f:
+        with open(score_file_path, "w") as f:
             json.dump(new_scores, f, indent=2)
     else:
         with open(score_file_path) as f:
             new_scores = json.load(f)
-        print(score_file_path,'exists')
+        print(score_file_path, "exists")
 
     if args.long_context:
-        key = 'gold_ids_long'
+        key = "gold_ids_long"
     else:
-        key = 'gold_ids'
+        key = "gold_ids"
     ground_truth = {}
     for e in raw_examples:
-        ground_truth[e['id']] = {}
+        ground_truth[e["id"]] = {}
         for gid in e[key]:
-            ground_truth[e['id']][gid] = 1
+            ground_truth[e["id"]][gid] = 1
         for i in e["excluded_ids"]:
             if i in documents:
-                ground_truth[e['id']][i] = 0
+                ground_truth[e["id"]][i] = 0
 
     # The import is here to prevent environment issues
     from retrievers import calculate_retrieval_metrics
 
-    results = calculate_retrieval_metrics(results=new_scores, qrels=ground_truth)
-    with open(os.path.join(outputs_path, "reranker_results.json"), 'w') as f:
+    results = calculate_retrieval_metrics(
+        results=new_scores, qrels=ground_truth
+    )
+    with open(os.path.join(outputs_path, "reranker_results.json"), "w") as f:
         json.dump(results, f, indent=2)
 
     # break ties by interpolating with the retriever scores
@@ -170,9 +228,15 @@ if __name__=='__main__':
     for qid in new_scores:
         retriever_interpolated_scores[qid] = {}
         for did in new_scores[qid]:
-            retriever_interpolated_scores[qid][did] = (0.5 * new_scores[qid][did]) + (0.5 * all_scores[qid][did])
-    results = calculate_retrieval_metrics(results=retriever_interpolated_scores, qrels=ground_truth)
-    with open(os.path.join(outputs_path, f"reranker_retriever_results.json"), 'w') as f:
+            retriever_interpolated_scores[qid][did] = (
+                0.5 * new_scores[qid][did]
+            ) + (0.5 * all_scores[qid][did])
+    results = calculate_retrieval_metrics(
+        results=retriever_interpolated_scores, qrels=ground_truth
+    )
+    with open(
+        os.path.join(outputs_path, f"reranker_retriever_results.json"), "w"
+    ) as f:
         json.dump(results, f, indent=2)
 
     # break ties by combining with the bm25 scores
@@ -183,7 +247,13 @@ if __name__=='__main__':
         for qid in new_scores:
             bm25_interpolated_scores[qid] = {}
             for did in new_scores[qid]:
-                bm25_interpolated_scores[qid][did] = (100 * new_scores[qid][did]) + bm25_scores[qid][did]
-        results = calculate_retrieval_metrics(results=bm25_interpolated_scores, qrels=ground_truth)
-        with open(os.path.join(outputs_path, f"reranker_bm25_results.json"), 'w') as f:
+                bm25_interpolated_scores[qid][did] = (
+                    100 * new_scores[qid][did]
+                ) + bm25_scores[qid][did]
+        results = calculate_retrieval_metrics(
+            results=bm25_interpolated_scores, qrels=ground_truth
+        )
+        with open(
+            os.path.join(outputs_path, f"reranker_bm25_results.json"), "w"
+        ) as f:
             json.dump(results, f, indent=2)
